182eb76113f4abba70df527a5fc684e9db3a6513
shoaib856 <miloshoaib@gmail.com>
2025-04-09 15:59:19 +0200

    feat: enhance Calculate page with localization support; update translations and integrate dynamic text rendering

--- src/app/calculate/page.tsx
+++ src/app/calculate/page.tsx
@@ -3,17 +3,19 @@ import Footer from "../components/Footer";
 import RadioElement from "../components/RadioElement";
 import Slider from "../components/Slider";
 import Input from "../components/Input";
+import { useTranslations } from "next-intl";
 
 const CalculatePage = () => {
+  const t = useTranslations();
   return (
     <main className="flex-1 main-bg-gradient flex-col flex">
       <div className="flex max-sm:flex-col pt-12 sm:pt-28 sm:ps-[88px] max-sm:gap-16 px-8">
         <h2 className="text-bg capitalize text-5xl tracking-tighter sm:max-w-[330px] font-bold shrink-0">
-          Architectural Design Calculator
+          {t("calculateTitle")}
         </h2>
         <div className="flex-1 flex gap-16 justify-center">
           <div className="max-w-md w-full">
-            <h4 className="text-white">Choose Architectural Design</h4>
+            <h4 className="text-white">{t("Choose Architectural Design")}</h4>
             <div className="mt-5 space-y-5">
               <div className="bg-linearGradient rounded">
                 <select
@@ -26,39 +28,39 @@ const CalculatePage = () => {
                 </select>
               </div>
               <div className="grid grid-cols-3 gap-8">
-                <RadioElement name="radio-1" label="Office Use" />
-                <RadioElement name="radio-1" label="Mixed Use" />
-                <RadioElement name="radio-1" label="Residential" />
+                <RadioElement name="radio-1" label={t("Office Use")} />
+                <RadioElement name="radio-1" label={t("Mixed Use")} />
+                <RadioElement name="radio-1" label={t("Residential")} />
               </div>
               <div className="border flex justify-center border-bg items-center py-2.5">
-                <p className="text-white uppercase text">building</p>
+                <p className="text-white uppercase text">{t("building")}</p>
               </div>
               <div className="grid grid-cols-3 gap-8">
-                <RadioElement name="radio-2" label="Mixed Use" />
-                <RadioElement name="radio-2" label="Residential" />
-                <RadioElement name="radio-2" label="Hospitality" />
+                <RadioElement name="radio-2" label={t("Mixed Use")} />
+                <RadioElement name="radio-2" label={t("Residential")} />
+                <RadioElement name="radio-2" label={t("Hospitality")} />
               </div>{" "}
               <div className="grid grid-cols-3 gap-8">
-                <RadioElement name="radio-2" label="Commercial" />
-                <RadioElement name="radio-2" label="Health Care" />
-                <RadioElement name="radio-2" label="Offices" />
+                <RadioElement name="radio-2" label={t("Commercial")} />
+                <RadioElement name="radio-2" label={t("Health Care")} />
+                <RadioElement name="radio-2" label={t("Offices")} />
               </div>
               <div className="border flex justify-center border-bg items-center py-2.5">
-                <p className="text-white uppercase text">industrial</p>
+                <p className="text-white uppercase text">{t("industrial")}</p>
               </div>
               <div className="grid grid-cols-3 gap-8">
-                <RadioElement name="radio-3" label="Warehouse" />
-                <RadioElement name="radio-3" label="factory" />
+                <RadioElement name="radio-3" label={t("Warehouse")} />
+                <RadioElement name="radio-3" label={t("factory")} />
               </div>
               <Slider />
             </div>
             <hr className="divider h-px border-white" />
             <div>
               <div className="text-lg font-medium mb-1 text-white">
-                Total Estimated Cost
+                {t("Total Estimated Cost")}
               </div>
               <p className="text-sm text-neutral-500">
-                pricing for all services is in saudi riyals.
+                {t("pricing for all services is in saudi riyals")}
               </p>
               <div className="bg-linearGradient w-fit p-2.5 mt-5">0000000</div>
             </div>
@@ -66,20 +68,20 @@ const CalculatePage = () => {
           <div className="divider after:bg-linearGradient before:bg-linearGradient divider-horizontal" />
           <div className="bg-white max-w-2xl w-full  px-5 py-8">
             <div role="grid" className="grid grid-cols-2 gap-x-4 gap-y-14 mb-4">
-              <Input label="First Name" type="text" />
-              <Input label="Last Name" type="text" />
-              <Input label="Telephone" type="tel" />
-              <Input label="Email" type="email" />
+              <Input label={t("First Name")} type="text" />
+              <Input label={t("Last Name")} type="text" />
+              <Input label={t("Telephone")} type="tel" />
+              <Input label={t("Email")} type="email" />
               <div className="col-span-full">
                 <label className="flex flex-col gap-4">
-                  <span className="uppercase">Message</span>
+                  <span className="uppercase">{t("Message")}</span>
                   <textarea className="border outline-0 p-2" rows={10} />
                 </label>
               </div>
             </div>
 
-            <button className="bg-linearGradient w-full uppercase font-bold text-lg py-2.5">
-              send inquiry
+            <button className="cursor-pointer bg-linearGradient w-full uppercase font-bold text-lg py-2.5">
+              {t("send inquiry")}
             </button>
           </div>
         </div>
--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 export default function PlotAreaSlider() {
   const [value, setValue] = useState(0);
+  const t = useTranslations();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(Number(e.target.value));
@@ -12,13 +14,13 @@ export default function PlotAreaSlider() {
     <div className="text-white w-full">
       {/* Header */}
       <div className="flex items-center justify-between mb-1">
-        <label className="text-lg font-medium">Total Plot Area</label>
-        <button className="bg-[#363636] text-xs px-2 py-1 rounded-md border border-neutral-500">
-          M²
-        </button>
+        <label className="text-lg font-medium">{t("Total Plot Area")}</label>
+        <div className="bg-[#363636] text-xs px-2 py-1 rounded-md border border-neutral-500">
+          m²
+        </div>
       </div>
       <p className="text-sm text-neutral-400 mb-2">
-        slide to select the space of the services in m².
+        {t("slide to select the space of the services in m2")}
       </p>
 
       {/* Slider */}
@@ -61,8 +63,8 @@ export default function PlotAreaSlider() {
 
       {/* Labels */}
       <div className="flex justify-between text-sm mt-1 text-white">
-        <span>01 m²</span>
-        <span>1000000 m²</span>
+        <span dir="ltr">01 m²</span>
+        <span dir="ltr">1000000 m²</span>
       </div>
     </div>
   );
